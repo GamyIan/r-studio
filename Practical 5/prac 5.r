3b61e699296bdabe79d3447cd0920a4609966670
Ian Reddy <73981521+GamyIan@users.noreply.github.com>
2025-02-13 08:37:26 +0000

    Fixed the order

---
+++ Practical 5/prac 5.r
@@ -0,0 +1,25 @@
+df = read.csv("Crimes_2022.csv")
+
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+
+#Monthwise no. of domestic crime
+df$Date = mdy_hms(df$Date)
+df$Month = month(df$Date,label=TRUE)
+mwd=df %>% filter(Domestic==TRUE) %>%
+  group_by(Month) %>%
+  summarize(crimes=n())
+
+# Display bar chart
+ggplot(mwd,
+       aes(x=Month,y=crimes)) +
+  geom_bar(stat="identity",color="BLACK",fill="darkgreen")
+
+#Draw Line chart
+ggplot(mwd, aes(x=Month,y=crimes,group=1)) +
+  geom_line(color="green",size=1) +
+  geom_point(color="red",size=1)
+
+
